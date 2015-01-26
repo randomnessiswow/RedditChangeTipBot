e3ab1a57dc0e69c6ac41f32d0a90079e3bdb915c
randomnessiswow <austino@vcn.com>
2015-01-26 11:54:19 -0700

    Some variable changes possibly...
    I changed 'for subreddit in subreddit' to 'for s in subreddit', it just might be easier to differentiate between the item that's being iterated over, and the iterating variable.

--- redditbot.py
+++ redditbot.py
@@ -12,15 +12,15 @@ def random_subreddit():
 	subreddit = r.get_popular_subreddits(limit=100)
 	sub=[]
 	rand =random.randrange(1,100)
-	for subreddit in subreddit:
-		sub.append(subreddit)
+	for s in subreddit:
+		sub.append(s)
 	return sub[rand]
 	
 subreddit = random_subreddit()
 print subreddit
 comments = subreddit.get_comments(limit=1)
-for comment in comments:
-	author= str(comment.author)
+for c in comments:
+	author= str(c.author)
 	print ("Merry Bitmass /u/"+author+". Here have some bits /u/ChangeTip")
 	#comment.reply("Merry Bitmass /u/"+author+". Here, have some bits /u/ChangeTip")
 
